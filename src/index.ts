22b04a612867a2d1e59fcf32d040e94cb51eb984
Jacob Wright <jacwright@gmail.com>
2025-04-30 18:43:01 -0600

    Fix exports

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 export { Delta } from '@dabble/delta';
 export * from './client/Patches.js';
 export * from './client/PatchesDoc.js';
-export type * from './event-signal';
-export type * from './json-patch/JSONPatch.js';
+export * from './event-signal';
+export * from './json-patch/JSONPatch.js';
 export type { ApplyJSONPatchOptions } from './json-patch/types.js';
 export type * from './persist/PatchesStore.js';
 export type * from './types';

d5a2c4a0b6dbacefe5a69ef2bae54c8bdc138db5
Jacob Wright <jacwright@gmail.com>
2025-05-16 15:46:36 -0600

    Export JSONPatch too

--- src/json-patch/index.ts
+++ src/json-patch/index.ts
@@ -5,5 +5,6 @@ export { applyBitmask, bitmask, combineBitmasks } from './ops/bitmask.js';
 export * as defaultOps from './ops/index.js';
 export { transformPatch } from './transformPatch.js';
 
+export * from './JSONPatch.js';
 export * from './ops/index.js';
 export type { ApplyJSONPatchOptions, JSONPatchOpHandlerMap as JSONPatchCustomTypes, JSONPatchOp } from './types.js';
